5c66bcfcfe0fb05c9ca606f9c5f1e0d21a48fe79
costanzapert <costanzapertosa97@gmail.com>
2025-01-31 16:24:36 +0100

    ho scoperto solo cose non fattibili

--- Esercizio_riassuntivo.py
+++ Esercizio_riassuntivo.py
@@ -1,15 +1,26 @@
 #Trova un numero con più di 5 divisori. Stampa i divisori di ogni numero in input,  se ne ha più di 5 fermati.
-#if, range, for , while, liste, variabili
+#if, range, for , while, liste, variabili, print
 divisori = []
 while len(divisori) < 5:
     divisori = []
     n = int(input("Inserisci numero: "))
     for i in range(1,n+1, 1):
         if n % i == 0:
-            divisori.append(i) 
+            divisori.append(i)             
     print(divisori)
+    #for i not in divisori.difference(range(1,n+1, 1)): 
+        
+
+
+
+
+# booleani , strighe, tuple, float, break, pass, continue, splat
+
+#trasforma le liste in tuple
     
+#divisori_tupla= (*divisori)
+#print(divisori_tupla)
 
 
 
-# booleani , strighe, tuple, float, break, pass, continue
+# ho scoperto cose su tuple che non funzionano
